ea252de76cf11eef81f86a7a700b4b7014fe972d
Bahrami <m.m.bhrm85@gmail.com>
2026-02-03 05:29:15 -0800

    Generate migrations.

---
+++ backend/src/Backend.Infra/Migrations/20260203132728_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "YsqForms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    Degree = table.Column<string>(type: "text", nullable: false),
+                    EducationMajor = table.Column<string>(type: "text", nullable: false),
+                    JobTitle = table.Column<string>(type: "text", nullable: false),
+                    Gender = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YsqForms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "YsqFormAnswer",
+                columns: table => new
+                {
+                    YsqFormId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YsqFormAnswer", x => new { x.YsqFormId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_YsqFormAnswer_YsqForms_YsqFormId",
+                        column: x => x.YsqFormId,
+                        principalTable: "YsqForms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "YsqFormAnswer");
+
+            migrationBuilder.DropTable(
+                name: "YsqForms");
+        }
+    }
+}
